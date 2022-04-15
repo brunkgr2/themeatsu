1e73834bf1e6b3dcd2744719208e7b9e0578302f
Mark Brunk-Grady <brunkgr2@uwm.edu>
2022-04-15 17:02:59 -0500

    create fix_names function

---
+++ R/fix_names.R
@@ -0,0 +1,24 @@
+#' A function to remove unwanted characters in a character vector
+#'
+#'@param x Character vector to be changed
+#'@param patterns Character vector of patterns to find given as c("pattern")
+#'@param replacements Character vector of replacements for patterns given as c("replacements")
+#'@param case Character vector of either "lower" or "upper" for the output case
+#'
+
+
+fix_names <- function(x, patterns, replacements, case){
+
+  x <- stringi::stri_replace_all_regex(x,
+                                       pattern = patterns,
+                                       replacement = replacements,
+                                       vectorize_all = FALSE)
+
+  if(case == "lower") {
+    x <- tolower(x)
+  }
+  else {
+    x <- toupper(x)
+  }
+
+}
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -2,3 +2,15 @@ test_that("random color function", {
   expect_equal(length(rand_pal(6)), 6)
   }
 )
+
+x <- names(iris)
+x <- tolower(x)
+x <- stringi::stri_replace_all_regex(x, pattern = ".", "_", vectorize_all = F)
+
+test_that("fix names", {
+  expect_equal(fix_names(names(iris),
+                         patterns = ".",
+                         replacements = "_",
+                         case = "lower"), x)
+  }
+)
